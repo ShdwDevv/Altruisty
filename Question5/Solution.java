b33613641af3a70341f0970dd6a2871b5dccd781
arshathahamed10 <arshathahamed500@gmail.com>
2024-09-08 13:29:03 +0530

    Question 5

--- Question5/Solution.java
+++ Question5/Solution.java
@@ -1,14 +1,6 @@
 import java.util.*;
 public class Solution{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter N : ");
-        int n = sc.nextInt();
-        int[] prices=new int[n];
-        for(int i = 0;i < n;i++) prices[i] = sc.nextInt();
-        System.out.println();
-        System.out.print("Enter Money : ");
-        int money = sc.nextInt();
+    public static int getMaxToys(int[] prices,int money,int n){
         int l = 0,r=0,sum=0,maxLen = 0;
         while(r < n){
             sum += prices[r];
@@ -19,7 +11,18 @@ public class Solution{
             maxLen = Math.max(maxLen,(r - l + 1));
             r++;
         }
-        System.out.println(maxLen);
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N : ");
+        int n = sc.nextInt();
+        int[] prices=new int[n];
+        for(int i = 0;i < n;i++) prices[i] = sc.nextInt();
+        System.out.println();
+        System.out.print("Enter Money : ");
+        int money = sc.nextInt();
+        System.out.println(getMaxToys(prices, money, n));
         sc.close();
     }
 }
